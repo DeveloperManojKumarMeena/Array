8c319869cd1bdfdbaa22f9153b4377ce114eb25b
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-01 08:03:54 +0530

    sort numbers in ascending and descending order

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ arr.sort()
 console.log(arr)
 
 let arr2= [10,2,5];
+//it's use for normal sort we can say string shorting...
 arr2.sort()
 console.log(arr2)
+//if we want sort number's we use this methode...
+
+//following methode for the ascending order
 console.log(arr2.sort((a,b)=>a-b))
+
+//and this sort code for descending order number sorting
+console.log(arr2.sort((a,b)=>b-a))
