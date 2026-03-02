ccffcde076e6417a9c7b6a3dfecc08c0e19c0fd6
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-02 17:33:33 +0530

    Happy Holi

--- map.js
+++ map.js
@@ -21,10 +21,21 @@
 // });
 // console.log(result)
 
-let arr = [1,2,3,4];
+let arr = [10,20,30,4];
 
-let result = arr.map((num, index) => {
-   return index;
-});
+// let result = arr.map((num, index) => {
+//    return index;
+// });
 
-console.log(result);
+// console.log(result);
+
+//Q. Sum of Array's Element
+
+let sum = 0;
+let max = 0
+arr.forEach((n)=>{
+   //sum +=n
+   if(max<n)max=n
+})
+// console.log('sum of array elements are : ' + sum)
+console.log('max element of array is :' + max)
