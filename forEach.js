eeab2dc36f2bd13937aba6d5e8d020d852055576
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-02 07:52:12 +0530

    forEach: Update forEach.js with new logic and logging

--- forEach.js
+++ forEach.js
@@ -1,6 +1,14 @@
-let arr = [1,2,3,4];
+ let arr = [1,2,3,4];
 
-arr.forEach((num, index) => {
-   if(num === 2) return;
-   console.log(num);
-});
+// arr.forEach((num, index) => {
+//    if(num === 2) return;
+//    console.log(num);
+// });
+const result = arr.forEach((n,i,a)=>{
+    n = n*i;
+    if(n==i+1)return
+    process.stdout.write(`n = ${n} i= ${i} a= ${a}`)
+    console.log()
+})
+
+console.log(result)
