29b8fc2e5b7168cdfadea13fcc5e18c255723ffa
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-07 17:42:29 +0530

    second max number in an array

---
+++ dsa.js
@@ -0,0 +1,14 @@
+let arr = [28,83,74,48,84,38];
+
+let max = Math.max(arr[0],arr[1]);
+let smax = Math.min(arr[0,arr[1]])
+
+for(let i = 2; i<arr.length;i++){
+    if(arr[i]>max){
+        smax = max ;
+        max = arr[i]
+    }else if(arr[i]>smax){
+        smax = arr[i]
+    }
+}
+console.log(`second max number ${smax}`)
