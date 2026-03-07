ece8bc8f2264f83b8737fadc1f75b157d96bc941
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-07 17:44:22 +0530

    second approach to find max number in array using spread operator and Math.max() function

--- dsa.js
+++ dsa.js
@@ -1,14 +1,18 @@
 let arr = [28,83,74,48,84,38];
 
-let max = Math.max(arr[0],arr[1]);
-let smax = Math.min(arr[0,arr[1]])
+// let max = Math.max(arr[0],arr[1]);
+// let smax = Math.min(arr[0,arr[1]])
 
-for(let i = 2; i<arr.length;i++){
-    if(arr[i]>max){
-        smax = max ;
-        max = arr[i]
-    }else if(arr[i]>smax){
-        smax = arr[i]
-    }
-}
-console.log(`second max number ${smax}`)
+// for(let i = 2; i<arr.length;i++){
+//     if(arr[i]>max){
+//         smax = max ;
+//         max = arr[i]
+//     }else if(arr[i]>smax){
+//         smax = arr[i]
+//     }
+// }
+// console.log(`second max number ${smax}`)
+
+let max = Math.max(...arr);
+
+console.log(max)
